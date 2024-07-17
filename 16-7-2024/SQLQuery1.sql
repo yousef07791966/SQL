ab374e96b85e1e49cea206468fb46a4e0b2540d7
yousef khraiwesh <yu.yousseff@gmail.com>
2024-07-17 16:44:14 +0300

    Add files via upload

--- 16-7-2024/SQLQuery1.sql
+++ 16-7-2024/SQLQuery1.sql
@@ -2,58 +2,54 @@ create database school_system ;
 use school_system
 
 create table Teachers(
-Teachers_id int ,
-Teachers_name varchar (15) ,
-Teachers_age int ,
-Teachers_lastname varchar (15) ,
-Teachers_valge varchar (15)
+Teachers_id int primary key  ,
+Teachers_name varchar (50) ,
 );
-
+--drop table Teachers;
+--drop table TeacherDetails;
 create table TeacherDetails(
 TeacherDetails_number int ,
-TeacherDetails_contry varchar (10),
-TeacherDetails_stret int ,
+Teachers_id int,
+TeacherDetails_contry varchar (50),
+TeacherDetails_stret varchar (50) ,
 TeacherDetails_birthdate int ,  
-);
+ FOREIGN KEY (Teachers_id) REFERENCES teachers(Teachers_id) )
 
 create table Courses (
-Courses_id int ,
+Courses_id int primary key ,
 Courses_name varchar (15),
 );
 
 create table Students (
-Students_id int ,
-Students_name varchar (15),
-Students_lastname varchar (15),
-Students_number int ,
-Students_email varchar (15)
+Students_id int primary key ,
+Students_name varchar (50),
 );
 
-insert into Teachers (Teachers_id, Teachers_name, Teachers_age, Teachers_lastname, Teachers_valge)
+insert into Teachers (Teachers_id, Teachers_name)
 values
-(1, 'yousef', 22, 'kh', 'amman') ,
-(2, 'mahmood', 27, 'aq', 'jerash') ,
-(3, 'yahya', 22, 'kt', 'irbed') , 
-(4, 'basil', 23, 'ms', 'aqapa') ,
-(5, 'moutaz', 28, 'ti', 'amman')
+(1, 'yousef') ,
+(2, 'mahmood') ,
+(3, 'yahya') , 
+(4, 'basil') ,
+(5, 'moutaz');
 
-insert into TeacherDetails(TeacherDetails_number, TeacherDetails_contry, TeacherDetails_stret ,TeacherDetails_birthdate)
+insert into TeacherDetails(TeacherDetails_number, Teachers_id, TeacherDetails_contry ,TeacherDetails_stret,TeacherDetails_birthdate)
 values 
-(01 , 'jordan' , 'alsaif' , 1999),
-(02 , 'jordan' , 'alsaif' , 1998),
-(03 , 'jordan' , 'alsaif' , 1997),
-(04 , 'jordan' , 'alsaif' , 1996)
+(1 , 1 , 'jotdan' ,'alyousef' ),
+(02 , 2 , 'jotdan' , 'alyousef'),
+(03 , 3 , 'jotdan' , 'alyousef'),
+(04 , 4 , 'jotdan' , 'alyousef')
 
 insert into Courses (Courses_id ,Courses_name)
 values 
-( 001 ,'C#' ) ,
+( 1 ,'C#' ) ,
 ( 002 , 'java') ,
 (003 , 'C++' ) ,
 (004 , 'SQL' )
 
-insert into Students ( Students_id , Students_name , Students_lastname , Students_number ,Students_email)
+insert into Students ( Students_id , Students_name )
 values 
-(00001 ,' ali ' ,'mo' , 1 , 'ali@gmail.com' ) , 
-(00002 ,' abdo ' ,'mo' , 2 , 'ali@gmail.com' ) , 
-(00003 ,' amro ' ,'mo' , 3 , 'ali@gmail.com' ) ,
-(00004 ,' ammar ' ,'mo' , 3 , 'ali@gmail.com' )
+(1 ,' ali ' ) , 
+(00002 ,' abdo ' ) , 
+(00003 ,' amro ' ) ,
+(00004 ,' ammar ');
